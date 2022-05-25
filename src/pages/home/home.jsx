4b1c29e337c72d674dc69b675f34685f838abbaa
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-05-25 00:16:37 -0500

    añadiend bitacora, swap outs, fallos de página y reemplazos

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -12,6 +12,7 @@ function Home(props) {
     const [fisDirs, setFisDirs] = useState([]);
 
     const [swapIns, setSwapIns] = useState([]);
+    const [swapOuts, setSwapOuts] = useState([]);
 
     const [frames, setFrames] = useState([])
     const [frameNumbers, setFrameNumbers] = useState([])
@@ -19,12 +20,15 @@ function Home(props) {
 
     const [modified, setModified] = useState([])
     const [pages, setPages] = useState([]);
+    const [frameAsigned, setFrameAsigned] = useState([]);
     const [valid, setValid] = useState([]);
-    
+
     const [indexDir, setIndexDir] = useState(0);
     const [indexFrames, setIndexFrames] = useState(0)
 
-    const [start, setStart] = useState(true)
+    const [pageFail, setPageFail] = useState(0);
+
+    const [log, setLog] = useState([]);
 
     let SOSize = 0;
     let processSize = 0;
@@ -44,33 +48,72 @@ function Home(props) {
         if (indexDir < directions.length) {
             const dir = directions[indexDir];
             const newFrames = [...frames]
+            const newValid = [...valid]
+            const newFrameAsigned = [...frameAsigned];
+            const newModified = [...modified];
 
             const page = Math.floor((dir) / frameSize);
-            const isPageLoaded = isPage(page);
 
             let frame;
             let fisDir;
             let swapIn;
+            let swapOut = "";
+            let newLog = "";
 
             if (!isPageOnFrames(page)) {
                 frame = indexFrames
+
+                if (frames[indexFrames] != 0) {
+                    newValid[frames[indexFrames]] = 0;
+                    newLog += `Se encontro un fallo de página en la página ${page} \n`
+                }
+
+                const lastPage = newFrames[frame];
+                if (lastPage !== "x") {
+                    newFrameAsigned[lastPage] = "";
+                    newLog += `Se saco la página ${lastPage} del marco ${frame}\n`
+                }
+
+
+                if (modified[lastPage] === 1) {
+                    swapOut = "x";
+                    modified[lastPage] = 0;
+                    newLog += `La pagina ${lastPage} fue modificada, se produjo un swap out \n`
+                }
+
                 newFrames[frame] = page;
-                setFrames(newFrames)
+                newFrameAsigned[page] = frame
+                newLog += `Se le asigno el marco ${frame} a la página ${page} \n`
+                setFrames(newFrames);
+
                 fisDir = indexFrames * frameSize + (dir % frameSize)
                 swapIn = "x";
+                newValid[page] = 1;
+                setPageFail(pageFail + 1)
+                setValid(newValid)
 
                 indexFrames >= (frames.length - 1) ? setIndexFrames(0) : setIndexFrames(indexFrames + 1);
             } else {
                 setFrames(newFrames)
+                newLog += `La página ${page} ya se encuentra en memoria \n`
                 frame = getFrameNumber(page);
                 fisDir = frame * frameSize + (dir % frameSize);
                 swapIn = "";
             }
 
+            if (operations[indexDir] === "E") {
+                newModified[page] = 1;
+                newLog += `La página ${page} fue modificada, su bit de modificado ahora es 1\n`
+            }
+
             setPageNumbers(pageNumbers.concat([page]));
             setFrameNumbers(frameNumbers.concat([frame]))
+            setFrameAsigned(newFrameAsigned);
+            setModified(newModified);
             setFisDirs(fisDirs.concat([fisDir]));
             setSwapIns(swapIns.concat([swapIn]));
+            setSwapOuts(swapOuts.concat([swapOut]));
+            setLog(log.concat([newLog]));
             setIndexDir(indexDir + 1);
         }
     }
@@ -95,20 +138,26 @@ function Home(props) {
     }
 
     const handleClick = () => {
-        const frameSize = parseInt(document.getElementById('frameSize').value)
+        // const frameSize = parseInt(document.getElementById('frameSize').value)
+        const frameSize = 4
         setFrameSize(frameSize);
-        setFrames(new Array(frameSize).fill(0))
+
+        const framesNumber = parseInt(document.getElementById('framesNumber').value)
+        setFrames(new Array(framesNumber).fill("x"))
 
         SOSize = document.getElementById('SOSize').value;
+
+        //processSize = document.getElementById('processSize').value;
+        processSize = 30;
+        const numPages = Math.ceil(processSize / frameSize)
         
-        processSize = document.getElementById('processSize').value;
-        const numPages = Math.ceil(processSize/frameSize)
         setPages(new Array(numPages).fill(0));
+        setFrameAsigned(new Array(numPages).fill(""));
         setValid(new Array(numPages).fill(0));
         setModified(new Array(numPages).fill(0));
-        
+
         setDirections(document.getElementById('directions').value);
-        
+
         setDirections([21, 26, 25, 6, 5, 29, 4, 5, 17, 16, 24, 25, 4, 7, 21, 1, 14, 15, 21, 22]);
         setOperations(["L", "L", "E", "E", "L", "L", "L", "E", "E", "E", "E", "L", "E", "L", "E", "E", "L", "L", "E", "E"]);
     }
@@ -146,6 +195,12 @@ function Home(props) {
 
             <div>
                 <button onClick={() => eventEmitter.emit('next')}>Siguiente</button>
+            </div>
+            <div>
+                <p><b>Fallos de página: </b> {pageFail}</p>
+            </div>
+            <div>
+                <p><b>Reemplazos: </b> {pageFail - frames.length > 0 ? pageFail - frames.length : 0}</p>
             </div>
             <div>
                 <table>
@@ -186,11 +241,17 @@ function Home(props) {
                                 swapIns.map((dir, index) => <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{dir}</motion.td>)
                             }
                         </tr>
+                        <tr>
+                            <td>Swap Out</td>
+                            {
+                                swapOuts.map((dir, index) => <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{dir}</motion.td>)
+                            }
+                        </tr>
                     </tbody>
                 </table>
             </div>
 
-            
+
             <div className='page-table-container'>
                 <table>
                     <thead>
@@ -213,7 +274,7 @@ function Home(props) {
                                 return (
                                     <tr>
                                         <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{index}</motion.td>
-                                        <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{valid[index]}</motion.td>
+                                        <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{frameAsigned[index]}</motion.td>
                                         <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{valid[index]}</motion.td>
                                         <motion.td initial="hidden" animate="visible" variants={variants} transition={{ duration: 1 }}>{modified[index]}</motion.td>
                                     </tr>
@@ -222,6 +283,20 @@ function Home(props) {
                         }
                     </tbody>
                 </table>
+            </div>
+
+            <div>
+                {
+                    log.map((log, index) => {
+                        return (
+                            <div>
+                                <p><b>Número de iteración: </b> {index}</p>
+                                <p style={{ "white-space": "pre-line" }}>{log}</p>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
         </div>
     )
